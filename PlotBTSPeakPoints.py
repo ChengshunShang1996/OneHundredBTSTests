22f1cad3a72572078deeb99cfaba8349a07f75da
Chengshun Shang <chengshun.shang1996@gmail.com>
2022-11-22 16:10:23 +0100

    update

--- PlotBTSPeakPoints.py
+++ PlotBTSPeakPoints.py
@@ -29,13 +29,16 @@ with open(BTS_peak_points_path_and_name, "r") as f_w_peak_points:
 '''
 #plt.contourf(X, Y, Z, 20, cmap=plt.get_cmap('YlGn'))
 #cs = plt.tricontour(x, y, z, levels=levels, colors = 'white', linewidths = 0.1)
-plt.tricontour(x, y, z, levels=[46193, 46195], colors = 'blue', linewidths = 0.5)
+plt.tricontour(x, y, z, levels=[46193, 46195], colors = 'white', linewidths = 0.5)
 plt.tricontourf(x, y, z, levels=levels, cmap='coolwarm')
 #cs.clabel(inline=True, fmt='%d', fontsize = 'smaller', manual=true)
 
 plt.xlabel('Tension_limit')
 plt.ylabel('Shear_limit')
 plt.title('BTS strength')
+
+#plt.xlim((0, 100000))
+#plt.ylim((0, 100000))
 
 plt.colorbar()
 plt.show()
